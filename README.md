efe96051806a864430dbc81d64b20fab895c1bdf
Thomas <thomas.kany8@etu.univ-lorraine.fr>
2025-10-03 10:46:30 +0200

    docker + remplissage bdd

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Sae5_Jardin_de_Cocagne
+
+
+
+- creation du projet git
+- Mise en place d'un symfony
+- mise en place de docker 
+- creation d'un docker compose
+- intitialisation de la bdd dans le .env 
+- creation d'un fichier nginx .conf
+- creation d'une page d'acceuil 
+- creation d'une page depot pour la BDD
---
+++ src/Entity/Depot.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DepotRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DepotRepository::class)]
+class Depot
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column]
+    private ?int $capacite = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $adresse = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $codePostal = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $ville = null;
+
+    #[ORM\Column(type: 'float')]
+    private ?float $longitude = null;
+
+    #[ORM\Column(type: 'float')]
+    private ?float $latitude = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+        return $this;
+    }
+
+    public function getCapacite(): ?int
+    {
+        return $this->capacite;
+    }
+
+    public function setCapacite(int $capacite): self
+    {
+        $this->capacite = $capacite;
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+        return $this;
+    }
+
+    public function getCodePostal(): ?string
+    {
+        return $this->codePostal;
+    }
+
+    public function setCodePostal(string $codePostal): self
+    {
+        $this->codePostal = $codePostal;
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): self
+    {
+        $this->ville = $ville;
+        return $this;
+    }
+
+    public function getLongitude(): ?float
+    {
+        return $this->longitude;
+    }
+
+    public function setLongitude(float $longitude): self
+    {
+        $this->longitude = $longitude;
+        return $this;
+    }
+
+    public function getLatitude(): ?float
+    {
+        return $this->latitude;
+    }
+
+    public function setLatitude(float $latitude): self
+    {
+        $this->latitude = $latitude;
+        return $this;
+    }
+}
+
